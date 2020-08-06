f5c5c591f0015c707ecd6e21a3448f61957b3067
Patthapong Niamkerd <aeknicotine@gmail.com>
2020-08-06 21:42:30 +0700

    Create InputController
    เชื่อม route กับ controller แบบมี parameter
    (Route Parameter)

---
+++ basic_laravel/app/Http/Controllers/InputController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class InputController extends Controller
+{
+    function inputName($name){
+        echo '<input type="text" name="name" id="name" value="'.$name.'">';
+    }//inputName
+
+    function showProduct($product){
+        echo "<script>alert('Show : '+ '$product')</script>";
+    }
+}
+
--- basic_laravel/routes/web.php
+++ basic_laravel/routes/web.php
@@ -16,3 +16,19 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/user',function (){
+    return '<h1>User : Patthapong</h1>';
+});
+
+Route::get('/showname',function(){
+    return '<b>Patthapong Naimkerd</b>';
+});
+
+Route::get('welcome/name','HelloController@showHello');
+//การเชื่อม route กับ Controller 
+// ระบุ ชื่อ Controller @ ชื่อ function ใน controller แทน  callback function
+
+
+Route::get('inputname/{name}','InputController@inputName');
+Route::get('showproduct/{prouct}','InputController@showProduct');
